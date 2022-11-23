ce68921014b0725a528b397c80645ba714d153b5
Roberto Moura <roblm_@hotmail.com>
2022-11-23 19:06:37 -0300

    05_Private_Object

--- 05_Private_Object/begin.js
+++ 05_Private_Object/begin.js
@@ -1,3 +1,19 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username]: "user",
+  [password]: "pass",
+  age: 29
+};
+
+// Private
+console.log(user.username)
+// Private
+console.log(user.password)
+// Public
+console.log(user.age)

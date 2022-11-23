1718f740975eb0661c1ed1d17ce2afd7fb9fa62c
Roberto Moura <roblm_@hotmail.com>
2022-11-23 19:10:49 -0300

    06_Ticking_Time

--- 06_Ticking_Time/begin.js
+++ 06_Ticking_Time/begin.js
@@ -1,3 +1,14 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function clock() {
+  const clockNode = document.querySelector("#clock");
+  return setInterval(() => {
+    let date = new Date();
+    let tick = date.toLocaleTimeString();
+    clockNode.textContent = tick;
+  }, 1000);
+};
+
+clock();

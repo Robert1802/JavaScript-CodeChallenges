b48de5b45c6c8592774cfa581f69169f41a21eed
Roberto Moura <roblm_@hotmail.com>
2022-11-23 19:17:53 -0300

    07_Classroom_Attendance

--- 07_Classroom_Attendance/begin.js
+++ 07_Classroom_Attendance/begin.js
@@ -1,3 +1,23 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function getStudents(classroom) {
+  // Atribute
+  //  hasTeachingAssistant <- hasTeachingAssistant
+  //  classList <- classList
+  let { hasTeachingAssistant, classList } = classroom;
+
+  if (hasTeachingAssistant) {
+    // destructure classList into teacher, assistant and students
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+  return students;
+}
+
+console.log(getStudents(  {
+  hasTeachingAssistant: false,
+  classList: ["Rashida", "John", "Roman", "Lisa", "Omair", "Lukas"],
+}));

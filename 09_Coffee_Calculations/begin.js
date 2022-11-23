8613d264980080e920e81dcc6924025fd6da148b
Roberto Moura <roblm_@hotmail.com>
2022-11-23 19:32:29 -0300

    09_Coffee_Calculations

--- 09_Coffee_Calculations/begin.js
+++ 09_Coffee_Calculations/begin.js
@@ -1,3 +1,16 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+const coffeePrice = 1.25;
+let totalBill = 0;
+
+function coffeeCalculator (coffeeArray) {
+  let coffeeSum = coffeeArray.reduce(
+    (totalCoffees, individualCoffee) => (totalCoffees += individualCoffee)
+  )
+  totalBill = coffeeSum * coffeePrice
+  return `The total bill was ${totalBill}`;
+}
+
+console.log(coffeeCalculator([2, 5, 7, 1, 4]));

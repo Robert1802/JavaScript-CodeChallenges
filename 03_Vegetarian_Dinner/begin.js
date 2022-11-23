fb9b0c41db6719f316311a51517d83fd96b91a2a
Roberto Moura <roblm_@hotmail.com>
2022-11-23 18:58:56 -0300

    03_Vegetarian_Dinner

--- 03_Vegetarian_Dinner/begin.js
+++ 03_Vegetarian_Dinner/begin.js
@@ -1,3 +1,39 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+const menu = [
+  {
+    name: "Chicken parmesan",
+    isVegetarian: false,
+  },
+  {
+    name: "Penne a la vodka",
+    isVegetarian: true,
+  },
+  {
+    name: "Mushroom risotto",
+    isVegetarian: true,
+  },
+  {
+    name: "Veal saltambuca",
+    isVegetarian: false,
+  },
+  {
+    name: "Filet mignon",
+    isVegetarian: false,
+  },
+];
+
+function vegetarianMenu(menu) {
+  const menuNode = document.querySelector("#menu");
+  const vegetarianOptions = menu.filter( (option) => option.isVegetarian === true);
+
+  vegetarianOptions.forEach((option) => {
+    let dish = document.createElement("li");
+    dish.textContent = option.name;
+    menuNode.appendChild(dish);
+  });
+}
+
+vegetarianMenu(menu);

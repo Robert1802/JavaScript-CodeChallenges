ebc918ab46c858ac1e0bcd801f15ebeabcc80e71
Roberto Moura <roblm_@hotmail.com>
2022-11-24 18:35:22 -0300

    10_Food_Truck

--- 10_Food_Truck/begin.js
+++ 10_Food_Truck/begin.js
@@ -1,3 +1,22 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function foodTruckFestival(menu) {
+  // combine the arrays into a single array
+  let flatMenu = menu.flat();
+
+  let newMenu = new Set();
+  flatMenu.forEach(menuItem => {
+    newMenu.add(menuItem);
+  });
+
+  const menuNode = document.querySelector("#combined-menu");
+  for (let item of newMenu){
+    let foodNode = document.createElement("li");
+    foodNode.innerText = item;
+    menuNode.appendChild(foodNode);
+  }
+}
+
+foodTruckFestival([["Tacos", "Burgers"], ["Pizza"], ["Burgers", "Fries"]]);

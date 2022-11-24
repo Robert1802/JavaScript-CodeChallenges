466f9954ef35d96cb5ede59aa844f06a8fff8435
Roberto Moura <roblm_@hotmail.com>
2022-11-24 18:57:19 -0300

    12_String_Permutation

--- 12_String_Permutation/begin.js
+++ 12_String_Permutation/begin.js
@@ -1,3 +1,28 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+// Recursion Solution
+function numPermutations(letters) {
+  if (letters.length === 1) {
+    return 1;
+  }
+
+  // slice cuts of the first element of the array.
+  return letters.length * numPermutations(letters.slice(1));
+}
+
+// // Iteration Solution
+// function numPermutations(letters) {
+//   let total;
+//   for (let i = 0; i < letters.length; i++) {
+//     if (total === undefined){
+//       total = 1;
+//       continue;
+//     }
+//     total *=1;
+//   }
+//   return total;
+// }
+
+console.log(numPermutations("hello"));

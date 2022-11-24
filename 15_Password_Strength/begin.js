2913d34dfb18422780ed3d39c5a37efccaf0a851
Roberto Moura <roblm_@hotmail.com>
2022-11-24 19:10:01 -0300

    15_Password_Strength

--- 15_Password_Strength/begin.js
+++ 15_Password_Strength/begin.js
@@ -1,3 +1,30 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+function checkPassword(password) {
+  let oneLowerCaseLetter = /(?=.+[a-z])/;
+  let oneUpperCaseLetter = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+  let minimumEightCharacters = /(?=.{8,})/;
+
+  // RegExp.test(string) checks if the string follows the rule of the regex
+  let isValid = 
+  oneLowerCaseLetter.test(password) &&
+  oneUpperCaseLetter.test(password) &&
+  oneDigit.test(password) &&
+  oneSpecialCharacter.test(password) &&
+  minimumEightCharacters.test(password);
+
+  // Ternary expression
+  return isValid
+    ? "Your password is valid"
+    : "Your password is invalid, try again";
+
+}
+
+// Test False
+console.log(checkPassword("abc"));
+// Test True
+console.log(checkPassword("9Ab!4567"));

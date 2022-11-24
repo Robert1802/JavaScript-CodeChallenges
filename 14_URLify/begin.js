8b38debf855ab889c2a433a983a85dc89db0134b
Roberto Moura <roblm_@hotmail.com>
2022-11-24 19:06:37 -0300

    14_URLify is regex

--- 14_URLify/begin.js
+++ 14_URLify/begin.js
@@ -1,3 +1,19 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+
+function urlify(blogTitle) {
+  // Create regex of forbidden characters
+  const punctuation = /[.,/#!$%^&*;:{}=!\-_`~()'"]/g; // /[regex]/
+  // const punctuation = new RegExp('.,/#!$%^&*;:{}=-_`~()\'"', 'g'); // "regex"
+  // const punctuation = new RegExp(/.,\/#!$%^&*;:{}=-_`~()'"/, 'g'); // "regex"
+
+  // replace ponctuations on the string with ""
+  const blogTitleWithoutPunctuation = blogTitle.replace(punctuation, "");
+  // replace space with - and remove trailing and leading
+  return blogTitleWithoutPunctuation.toLowerCase().trim().replaceAll(" ", "-");
+}
+
+console.log(urlify("How I Got Into Programming!!!"));
+console.log(urlify("I've got a new job :)"));

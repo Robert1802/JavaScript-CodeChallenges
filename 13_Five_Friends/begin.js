456ebd73bb3cfdada2e50d36c35d86f9998a43ef
Roberto Moura <roblm_@hotmail.com>
2022-11-24 19:02:29 -0300

    13_Five_Friends

--- 13_Five_Friends/begin.js
+++ 13_Five_Friends/begin.js
@@ -1,3 +1,27 @@
 /*
   WRITE YOUR SOLUTION HERE
 */
+
+// Named Ascync Function
+async function getUsers() {
+  let people = await fetch("https://randomuser.me/api/?results=5");
+  let data = await people.json();
+  const timeline = document.querySelector("#timeline");
+  data.results.forEach((person) => {
+    let image = document.createElement("img");
+    image.src = person.picture.medium;
+    timeline.appendChild(image);
+  });
+}
+
+// Anonymous Asynchronous Function
+// const getUsers = async () => {
+//   let people = await fetch("https://randomuser.me/api/?results=5");
+//   let data = await people.json();
+//   const timeline = document.querySelector("#timeline");
+//   data.results.forEach(person => {
+//     let image = document.createElement("img");
+//     image.src = person.picture.medium;
+//     timeline.appendChild(image);
+//   });
+// };
